ec8f2edfaa3c9d5debb998a02e71d7b85664e6d4
callelimascca <callelimasccamaria13@gmail.com>
2024-10-30 13:01:46 -0500

    feat: solucion a ejercicio 4

---
+++ 4. ImprimirArreglo/ejercicio.js
@@ -0,0 +1,14 @@
+// escribe tu respuesta acá
+function imprimirArreglo(arreglo) {
+    // Usar un bucle for para iterar sobre cada elemento del arreglo
+    for (let i = 0; i < arreglo.length; i++) {
+        console.log(arreglo[i]); // Imprimir cada elemento
+    }
+}
+
+// código de prueba
+console.log(imprimirArreglo(1, "Hola", 2, "Mundo"))
+// 1
+// Hola
+// 2
+// Mundo
