18aef9cace1cd125b32e052b17c0ff0c182ef212
callelimascca <callelimasccamaria13@gmail.com>
2024-10-30 12:15:30 -0500

    feat: solucion del ejercicio 1

---
+++ 1. ContraseñaValida/ejercicio.js
@@ -0,0 +1,9 @@
+// escribe tu respuesta acá
+function contraseñaValida(contraseña) {
+    return contraseña === "2Fj(jjbFsuj" || contraseña === "eoZiugBf&g9";
+}
+// código de prueba
+console.log(contraseñaValida("2Fj(jjbFsuj")) // true
+console.log(contraseñaValida("eoZiugBf&g9")) // true
+console.log(contraseñaValida("hola")) // false
+console.log(contraseñaValida("")) // false
